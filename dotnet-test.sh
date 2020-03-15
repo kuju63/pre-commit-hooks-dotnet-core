6a03bfe64a906d5adf9e7ed001a963ed9dd05913
Kurihara Jun <lh182051+src@gmail.com>
2020-03-15 12:34:17 +0900

    Add hook of executing dotnet test (#5)
    * Add new hook for executing dotnet test
    
    This commit is to add the new hook which execute dotnet test command.
    This hook is to need to have a argument "-p" or "--project".
    It option is test project path
    or  solution file path of including test project.
    
    * Modify filter setting
    
    This commit is to modify filter files setting to all csharp and vb file,
    because it hook is to need to execute when source code is changed.
    
    * Fix bug of filter setting.
    
    This commit is to fix the bug of including *.

---
+++ dotnet-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PARAMS="--nologo -v m"
+for OPT in "$@"
+do
+    case $OPT in
+        -p | --project)
+            if [[ -z "$2" ]]; then
+                exit 1
+            fi
+            PROJECT=$2
+        *)
+            shift 1
+            ;;
+    esac
+done
+
+if [[ -z "$PROJECT" ]]; then
+    echo "-p option is requirement option."
+    exit 1
+fi
+exec dotnet test $PARAMS $PROJECT
